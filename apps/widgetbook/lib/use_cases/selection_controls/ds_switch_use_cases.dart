ae47257f0b51716b05eb51117f4e8a1e7c655172
dhiana <dhianaslv@outlook.com>
2026-06-14 18:35:08 -0300

    fix: use local state for checkbox and switch

--- apps/widgetbook/lib/use_cases/selection_controls/ds_switch_use_cases.dart
+++ apps/widgetbook/lib/use_cases/selection_controls/ds_switch_use_cases.dart
@@ -21,7 +21,6 @@ Widget dsSwitchPlaygroundUseCase(BuildContext context) {
     label: 'Error text',
     initialValue: '',
   );
-  final value = context.knobs.boolean(label: 'On');
   final enabled = context.knobs.boolean(label: 'Enabled', initialValue: true);
   final size = context.knobs.object.dropdown<DSSwitchSize>(
     label: 'Size',
@@ -30,18 +29,58 @@ Widget dsSwitchPlaygroundUseCase(BuildContext context) {
     labelBuilder: (size) => size.name,
   );
 
-  return Center(
-    child: ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 320),
-      child: DSSwitch(
-        value: value,
-        label: label,
-        helperText: helperText.isEmpty ? null : helperText,
-        errorText: errorText.isEmpty ? null : errorText,
-        enabled: enabled,
-        size: size,
-        onChanged: enabled ? (_) {} : null,
-      ),
-    ),
+  return _SwitchPlayground(
+    label: label,
+    helperText: helperText.isEmpty ? null : helperText,
+    errorText: errorText.isEmpty ? null : errorText,
+    enabled: enabled,
+    size: size,
   );
+}
+
+class _SwitchPlayground extends StatefulWidget {
+  const _SwitchPlayground({
+    required this.label,
+    required this.enabled,
+    required this.size,
+    this.helperText,
+    this.errorText,
+  });
+
+  final String label;
+  final String? helperText;
+  final String? errorText;
+  final bool enabled;
+  final DSSwitchSize size;
+
+  @override
+  State<_SwitchPlayground> createState() => _SwitchPlaygroundState();
+}
+
+class _SwitchPlaygroundState extends State<_SwitchPlayground> {
+  bool value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 320),
+        child: DSSwitch(
+          value: value,
+          label: widget.label,
+          helperText: widget.helperText,
+          errorText: widget.errorText,
+          enabled: widget.enabled,
+          size: widget.size,
+          onChanged: widget.enabled
+              ? (newValue) {
+                  setState(() {
+                    value = newValue;
+                  });
+                }
+              : null,
+        ),
+      ),
+    );
+  }
 }
